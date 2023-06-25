73ca658faaa24c96b1b92ae302e97578fbe588db
Rob Harding <robbie.harding@hotmail.com>
2023-06-25 13:24:54 -0400

    start dynamic post detail page

---
+++ src/components/post-vote/PostVoteServer.tsx
@@ -0,0 +1,50 @@
+import { Post, Vote, VoteType } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import { FC } from "react";
+import PostVoteClient from "./PostVoteClient";
+
+interface PostVoteServerProps {
+  postId: string;
+  initialVotesAmt?: number;
+  initialVote?: VoteType | null;
+  getData?: () => Promise<(Post & { votes: Vote[] }) | null>;
+}
+
+const PostVoteServer = async ({
+  postId,
+  initialVotesAmt,
+  initialVote,
+  getData,
+}: PostVoteServerProps) => {
+  const session = await getServerSession();
+
+  let _votesAmt: number = 0;
+  let _currentVote: VoteType | null | undefined = undefined;
+
+  if (getData) {
+    const post = await getData();
+    if (!post) return notFound();
+
+    _votesAmt = post.votes.reduce(
+      (acc, vote) => (vote.type === "UP" ? acc + 1 : acc - 1),
+      0
+    );
+    _currentVote = post.votes.find(
+      (vote) => vote.userId === session?.user.id
+    )?.type;
+  } else {
+    _votesAmt = initialVotesAmt ?? 0;
+    _currentVote = initialVote;
+  }
+
+  return (
+    <PostVoteClient
+      initialVotesAmt={_votesAmt}
+      initialVote={_currentVote}
+      postId={postId}
+    />
+  );
+};
+
+export default PostVoteServer;
