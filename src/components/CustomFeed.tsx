57b5b355e45de858f2ef4e1e9bf554ffaae66b18
Rob Harding <robbie.harding@hotmail.com>
2023-06-25 08:53:45 -0400

    Homepage feed with and without login

---
+++ src/components/CustomFeed.tsx
@@ -0,0 +1,37 @@
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+import { db } from '@/lib/db'
+import PostFeed from './PostFeed'
+import { getAuthSession } from '@/lib/auth'
+
+const CustomFeed = async () => {
+  const session = await getAuthSession();
+
+  const subscriptions = await db.subscription.findMany({
+    where: {
+      userId: session?.user.id
+    },
+    select: {
+      subredditId: true
+    }
+  }).then((subs) => subs.map((sub) => sub.subredditId))
+
+  const posts = await db.post.findMany({
+    orderBy: { createdAt: 'desc' },
+    include: {
+      votes: true,
+      author: true,
+      comments: true,
+      subreddit: true,
+    },
+    where: {
+      subredditId: {
+        in: subscriptions
+      }
+    },
+    take: INFINITE_SCROLLING_PAGINATION_RESULTS
+  }) 
+
+  return <PostFeed initialPosts={posts}  />
+}
+
+export default CustomFeed
