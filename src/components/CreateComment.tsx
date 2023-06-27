7f4373e2d7e55d4e4f01fd94fdd1f5a7e75246ad
Rob Harding <robbie.harding@hotmail.com>
2023-06-27 11:07:38 -0400

    Working on text area

--- src/components/CreateComment.tsx
+++ src/components/CreateComment.tsx
@@ -1,15 +1,26 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useState } from "react";
 import { Label } from "./ui/Label";
-import { Textarea } from "./ui/textarea";
+import { Textarea } from "./ui/Textarea";
 
 interface CreateCommentProps {}
 
 const CreateComment: FC<CreateCommentProps> = ({}) => {
+  const [input, setInput] = useState<string>("");
+
   return (
     <div className="grid w-full gap-1.5">
       <Label htmlFor="comment">Your comment:</Label>
       <div className="mt-2">
-        <Textarea id="comment" />
+        <Textarea
+          id="comment"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          rows={1}
+          style={{ minHeight: "auto" }}
+          placeholder="What are your thoughts?"
+        />
       </div>
     </div>
   );
