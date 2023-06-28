2fd1c7f7ca0b51b8421f9a7815f2426919c1687e
Rob Harding <robbie.harding@hotmail.com>
2023-06-28 10:14:12 -0400

    Basic commenting functionality

--- src/components/CommentSection.tsx
+++ src/components/CommentSection.tsx
@@ -30,7 +30,7 @@ const CommentSection = async ({ postId }: CommentSectionProps) => {
 
   return (
     <div className="flex flex-col gap-y-4 mt-4">
-      <CreateComment />
+      <CreateComment postId={postId} />
 
       <div className="flex flex-col gap-y-6 mt-4">
         {comments
--- src/components/CreateComment.tsx
+++ src/components/CreateComment.tsx
@@ -3,11 +3,62 @@
 import { FC, useState } from "react";
 import { Label } from "./ui/Label";
 import { Textarea } from "./ui/Textarea";
+import { Button } from "./ui/Button";
+import { useMutation } from "@tanstack/react-query";
+import { CommentCreationRequest } from "@/lib/validators/comment";
+import axios, { AxiosError } from "axios";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { toast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
 
-interface CreateCommentProps {}
+interface CreateCommentProps {
+  postId: string;
+}
 
-const CreateComment: FC<CreateCommentProps> = ({}) => {
+const CreateComment: FC<CreateCommentProps> = ({ postId }) => {
   const [input, setInput] = useState<string>("");
+  const { loginToast } = useCustomToast();
+  const router = useRouter();
+
+  const { mutate: submitComment } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentCreationRequest) => {
+      const payload: CommentCreationRequest = {
+        postId,
+        text,
+        replyToId,
+      };
+
+      const { data } = await axios.patch(
+        "/api/subreddit/post/comment",
+        payload
+      );
+
+      return data;
+    },
+    onError(err) {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast();
+        } else if (err.response?.status == 400) {
+          return toast({
+            title: "You must be subscribed.",
+            description:
+              "You must be subscribed to this subreddit to comment on this post.",
+            variant: "destructive",
+          });
+        }
+      }
+
+      return toast({
+        title: "There was a problem.",
+        description: "Something went wrong. please try again later",
+        variant: "destructive",
+      });
+    },
+    onSuccess() {
+      router.refresh();
+    },
+  });
 
   return (
     <div className="grid w-full gap-1.5">
@@ -21,6 +72,20 @@ const CreateComment: FC<CreateCommentProps> = ({}) => {
           style={{ minHeight: "auto" }}
           placeholder="What are your thoughts?"
         />
+
+        <div className="mt-2 flex justify-end">
+          <Button
+            onClick={() =>
+              submitComment({
+                postId,
+                text: input,
+                replyToId: undefined,
+              })
+            }
+          >
+            Post
+          </Button>
+        </div>
       </div>
     </div>
   );
