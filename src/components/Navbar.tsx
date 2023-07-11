1791d0541c9a40542bb9484c8370f4d676c2ee9a
Rob Harding <robbie.harding@hotmail.com>
2023-07-11 10:39:16 -0400

    Try and fix mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,8 @@ const Navbar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-10 py-2">
-      <div className="container max-w-7x h-full mx-auto flex items-center justify-between gap-2">
+    <div className="fixed top-0 inset-x-0 max-h-max bg-zinc-100 border-b border-zinc-300 z-10 py-2">
+      <div className="container max-w-7x mx-auto flex items-center justify-between gap-2">
         {/* logo */}
         <Link href="/" className="flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
