7ac1a261f759a6b306dff390dba85115beca3311
Rob Harding <robbie.harding@hotmail.com>
2023-07-11 11:51:16 -0400

    Responsive styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ const Navbar = async () => {
 
   return (
     <div className="fixed top-0 inset-x-0 max-h-max bg-zinc-100 border-b border-zinc-300 z-10 py-2">
-      <div className="container max-w-7x mx-auto flex items-center justify-between gap-4 sm:gap-2">
+      <div className="sm:container px-1 max-w-7xl mx-auto flex items-center justify-between gap-6">
         {/* logo */}
         <Link href="/" className="flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
