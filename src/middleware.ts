6ec3c5dd2ef6cc35b17e7ca8c8bdb540cfe4fea7
Rob Harding <robbie.harding@hotmail.com>
2023-07-11 10:03:23 -0400

    Remove clog

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,8 +4,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req: NextRequest) {
   const token = await getToken({ req });
 
-  console.log(token);
-
   if (!token) {
     return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
   }
